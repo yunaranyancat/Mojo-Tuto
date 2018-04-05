14e9cc485cb44052120ed56ba36f4ce5cf571396
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-04-05 22:49:45 +0800

    send logging data to log file

---
+++ ex2_2.pl
@@ -0,0 +1,11 @@
+use Mojolicious::Lite;
+
+get '/:name' => {name => 'Default'} => sub {
+  my $self = shift;
+
+  my $name = $self ->param("name");
+
+  $self->render(text => "Hello world: $name");
+};
+
+app->start;
